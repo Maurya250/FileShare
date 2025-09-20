6bb974b198dd903a0f3b5ebf02bb97892c96c967
Maurya250 <aniketmaurya.dev@gmail.com>
2025-09-21 02:33:44 +0530

    change File

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
@@ -8,7 +9,7 @@ const authRoutes = require('./routes/auth');
 const fileRoutes = require('./routes/files');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -41,3 +42,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
